3b331f8d281e158f3ba1f5a89949940619c4b4e8
dhanushka4 <dhanushkajayaweera4@gmail.com>
2017-04-14 10:16:33 +0530

    adding 3 files together

--- edu1.php
+++ edu1.php
@@ -9,6 +9,8 @@
 echo "Success to me is working from home as a freelance web designer so I can spend
 more time with my family";
 
+echo "Success to me is being able to travel anywhere in the world, while maintaining
+my consulting business from wherever I am";
 
 ?>
 
